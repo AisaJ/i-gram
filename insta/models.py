187ce9654164c6063e6eebeecc5ea930d1a1f70f
Aisa <aisajemila@gmail.com>
2019-05-17 11:48:22 +0300

    added settings to configures storage of uploaded image files and static.

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+  profile_pic = models.ImageField(upload_to='image/',default='me.png')
+  bio = models.TextField()
+
+  def __str__(self):
+    return self.bio
+
+class Image(models.Model):
+  image = models.ImageField(upload_to='image/',default='arusha.jpeg')
+  name = models.CharField(max_length=40)
+  caption = models.TextField()
+  profile = models.ForeignKey(Profile)
+  likes = models.IntegerField()
+  comments = models.TextField()
+
+  def __str__(self):
+    return self.name
--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
+from .models import Profile,Image
 
 # Create your views here.
