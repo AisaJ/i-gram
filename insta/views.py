666e2c127cfeecd49c3252839d313f615e152b2b
Aisa <aisajemila@gmail.com>
2019-05-17 16:50:08 +0300

    Added urls.py for url path configurations. Added static file for css and js files.

--- insta/views.py
+++ insta/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
 from .models import Profile,Image
 
 # Create your views here.
+def home(request):
+  return render(request,'home.html')
