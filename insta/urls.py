80055d0ab47d1499997b7e0b70ceeadd29090ece
Aisa <aisajemila@gmail.com>
2019-05-20 14:42:21 +0300

    Added template files for displaying adding forms.

--- insta/urls.py
+++ insta/urls.py
@@ -6,7 +6,9 @@ from django.conf.urls.static import static
 urlpatterns=[
   url(r'^$',views.home,name='home'),
   url(r'^accounts/',include('registration.backends.simple.urls')),
-  url(r'^user/',views.user_profile,name='userProfile')
+  url(r'^user/',views.user_profile,name='userProfile'),
+  url(r'^new/post$',views.new_post,name='newPost'),
+  url(r'^profile$'),views.profile,name='profile'),
 ]
 if settings.DEBUG:
   urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- insta/views.py
+++ insta/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 from .models import Profile,Image
 from .forms import NewImageForm,NewProfileForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -12,5 +13,21 @@ def user_profile(request):
   post = Image.objects.all()
 
   return render(request,'insta.html',{"profile":profile,"post":post})
+
+@login_required(login_url='/accounts/login')
+def new_post(request):
+  current_user = request.user
+  if request.method == 'POST':
+    form = NewImageForm(request.POST,request.FILES)
+    if form.is_valid():
+      post = form.save(commit=False)
+      post.profile = current_user
+      post.save()
+    return redirect('userProfile')
+
+  else:
+    form = NewImageForm()
+  return render(request,'new_post.html',{"form":form})
+
 
 
