9fbeb536191b2f66b1ac3667b6ef625194cdf50c
Aisa <aisajemila@gmail.com>
2019-05-22 14:31:28 +0300

    Created tests for Image and Profile objects. setting up instance for testing.

--- insta/tests.py
+++ insta/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
-
+from  .models Image,Profile
 # Create your tests here.
+
+class ImageTestClass(TestCase):
+  def setUp(self):
+    self.holiday = Image(image='jinja.jpeg',name='jinja',caption='A visit to our neighbouring country',likes='2',comments='Looks adventurous')
+    self.holiday.save()
+
+  def test_instance(self):
+    self.assetTrue(isinstance(self.holiday,Image))
+
+  def tearDown(self):
+    Image.objects.all().delete()
+
+class ProfileTestClass(TestCase):
+  def setUp(self):
+    self.Jem = Profile(profile_pic='me.jpeg',bio='Always being awesome!')
+    self.Jem.save()
+
+  def test_instance(self):
+    self.assertTrue(isinstance(self.Jem,Profile))
+
+  def tearDown(self):
+    Profile.objects.all().delete()
